14c21b48ea5a9f21b3d9e5d6619c23aaa602e7ff
tobiasz-p <Tobiasz@DESKTOP-CK0MGLO>
2021-03-21 11:49:46 +0100

    Add returning products as objects

--- index.js
+++ index.js
@@ -16,63 +16,71 @@ function downloadPage(url) {
     });
 }
 
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; 
+        var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
 async function myBackEndLogic() {
     var links = new Array();
     var page = 1;
     var hasNextPage = true;
-    while (page < 2){ // hasNextPage
+    while (hasNextPage){ // hasNextPage
         try {
+            console.log("Getting products from page: " + page);
             const body = await downloadPage('https://winnicalidla.pl/alkohole-mocne.html?p=' + page)
-
-            // console.log(html);
             var $ = cheerio.load(body);
-            // fs.appendFileSync('body.html', body);
-            
             hasNextPage = $('a.i-next').length > 0;
 
-            // console.log($('li.col-lg-12').length)
             $('li.col-lg-12').each(function( index ) {
                 var link = $(this).find('a').attr('href');
-                // fs.appendFileSync('links.txt', link + '\n');
                 links.push(link);
-                //console.log(link);
             });
-
-            console.log("Crawled page: " + page);
             page++;
-    
         } catch (error) {
             console.error('ERROR:');
             console.error(error);
         }
     }
-    // console.log(links)
-    var products = new Array;
+
+    var products = [];
+
     for (let link of links) {
-        var product = new Array;
-        var re = /..(?=%'\;)/
-        console.log(link)
+        var re = /[0-9,]+(?=%)/
+        console.log("Processing " + link)
         const body = await downloadPage(link)
-        // fs.appendFileSync('body1.html', body);
         var $ = cheerio.load(body);
-        var alcohol = $('div.alcohol ').find('style').html(); // find style of div.alcohol
-        alcohol = alcohol.substr(alcohol.search(re), 2); // find regexp and get substring
-        var price = $('div.price-info').after('br').first().text().match(/[0-9]+,[0-9]{2}/)[0].replace(',', '.');
-        // var capacity = $('div.capacity').html();
-        // var subPrice = $('span.sub-price').html();
-        // console.log(alcohol);
-        // console.log(price);
-        // console.log(subPrice);
-        // console.log(capacity.match(/[0-9]+/)[0]);
-        
+
+        try {
+            var alcohol = $('div.alcohol ').find('style').html(); // find style of div.alcohol
+            alcohol = alcohol.substr(alcohol.search(re), 2).replace(',', '.'); // find regexp and get substring and replace ',' with '.'
+            console.log(alcohol);
+            var price = $('div.price-info').after('br').first().text().match(/[0-9]+,[0-9]{2}/)[0].replace(',', '.');
+            var name = $('h1.product-name').text();
+        }
+        catch (error) {
+        console.error('ERROR:');
+        console.error(error);
+        continue;
+    }   
         var pricePer100g = parseFloat(price) * 10 / parseFloat(alcohol);
-        // console.log(pricePer100g);
-        product.push(link, price, pricePer100g);
+        var product = {};
+        product.name = name;
+        product.link = link;
+        product.price = parseFloat(price);
+        product.pricePer100g = parseFloat(pricePer100g.toFixed(2));
         products.push(product);
     }
-
+    sortByKey(products, 'pricePer100g')
     console.log(products);
+    return products;
+    // fs.appendFileSync('products.txt', products);
 }
+
 
 // run your async function
 myBackEndLogic();
